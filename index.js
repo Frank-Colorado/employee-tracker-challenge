c555ac7844b2a1fe495d9d1f70a0cb5d4635ee73
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 23:33:15 -0400

    created options array

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+
+const options = [
+  "View All Employees",
+  "View All Departments",
+  "View All Roles",
+  "Add Employee",
+  "Add Department",
+  "Add Role",
+  "Update Employee Role",
+  "Quit",
+];
