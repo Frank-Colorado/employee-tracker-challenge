c2c40baa61da5e2cb844b77fbaedda134ad571d8
Frank-Colorado <chawnkiee@gmail.com>
2023-05-26 00:10:54 -0400

    created mainPrompt in prompts.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 const inquirer = require("inquirer");
 const handler = require("../utils/handler");
-
-// This is an array called options that contains the options that will be displayed to the user
-const options = [
-  "View All Employees",
-  "View All Departments",
-  "View All Roles",
-  "Add Employee",
-  "Add Department",
-  "Add Role",
-  "Update Employee Role",
-  "Quit",
-];
+const { mainPrompt } = require("../utils/prompts");
 
 // This is an async function called init
 // This will be the primary function that will be called to start the application
 const init = async () => {
   // This is an object called answer that contains the user's choice
-  const answer = await inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "Select an option:",
-      choices: options,
-    },
-  ]);
+  const answer = await inquirer.prompt(mainPrompt);
   // This is a variable called choice that contains the user's choice
   const choice = answer.choice;
   // The handler function is called and passed the user's choice
---
+++ utils/prompts.js
@@ -0,0 +1,18 @@
+// This is an array called options that contains the options that will be displayed to the user
+const mainPrompt = [
+  {
+    type: "list",
+    name: "choice",
+    messsage: "Select an option:",
+    choices: [
+      "View All Employees",
+      "View All Departments",
+      "View All Roles",
+      "Add Employee",
+      "Add Department",
+      "Add Role",
+      "Update Employee Role",
+      "Quit",
+    ],
+  },
+];
