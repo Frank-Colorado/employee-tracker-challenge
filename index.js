2a318009427f85ef64e1e4687f19ecc8ce2e09f2
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 23:43:44 -0400

    created init function in index.js file

--- index.js
+++ index.js
@@ -10,3 +10,53 @@ const options = [
   "Update Employee Role",
   "Quit",
 ];
+
+const init = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "Select an option:",
+      choices: options,
+    },
+  ]);
+  const choice = answer.choice;
+
+  switch (choice) {
+    case "View All Employees":
+      // call function
+      console.log("View All Employees");
+      break;
+    case "View All Departments":
+      // call function
+      console.log("View All Departments");
+      break;
+    case "View All Roles":
+      // call function
+      console.log("View All Roles");
+      break;
+    case "Add Employee":
+      // call function
+      console.log("Add Employee");
+      break;
+    case "Add Department":
+      // call function
+      console.log("Add Department");
+      break;
+    case "Add Role":
+      // call function
+      console.log("Add Role");
+      break;
+    case "Update Employee Role":
+      // call function
+      console.log("Update Employee Role");
+      break;
+    case "Quit":
+      console.log("Goodbye!");
+      process.exit(0);
+  }
+
+  init();
+};
+
+init();
