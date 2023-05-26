5ce4850fd3409eae4a88d6b0f2bce15cba4ce5b6
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 23:55:00 -0400

    commented the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const handler = require("./utils/handler");
 
+// This is an array called options that contains the options that will be displayed to the user
 const options = [
   "View All Employees",
   "View All Departments",
@@ -12,7 +13,10 @@ const options = [
   "Quit",
 ];
 
+// This is an async function called init
+// This will be the primary function that will be called to start the application
 const init = async () => {
+  // This is an object called answer that contains the user's choice
   const answer = await inquirer.prompt([
     {
       type: "list",
@@ -21,8 +25,11 @@ const init = async () => {
       choices: options,
     },
   ]);
+  // This is a variable called choice that contains the user's choice
   const choice = answer.choice;
+  // The handler function is called and passed the user's choice
   handler(choice);
+  // Recursion is implemented to call the init function again until the user quits
   init();
 };
 
