25133c196bc8591c0ea893474eee6d1826cd9a09
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 22:24:10 -0400

    added base code to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const routes = require("./routes");
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use(routes);
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
