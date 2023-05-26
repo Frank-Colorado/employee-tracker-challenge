677dbe456e2f72b113c395e40f739d00af00ff8e
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 22:39:04 -0400

    created db schema in schemas.sql file

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
 const routes = require("./routes");
 
 // Middlewares
