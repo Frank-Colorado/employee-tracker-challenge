6b929c0b7f763866b844e30055c7909287bdd438
Frank-Colorado <chawnkiee@gmail.com>
2023-05-26 17:19:57 -0400

    added async create method in Employee Class

--- modules/employee.js
+++ modules/employee.js
@@ -7,4 +7,19 @@ class Employee {
     this.roleId = roleId;
     this.managerId = managerId;
   }
+  async create() {
+    try {
+      const [result] = await connection.query(
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+        [this.firstName, this.lastName, this.roleId, this.managerId]
+      );
+      const [employee] = await connection.query(
+        "SELECT * FROM employee WHERE id = (?)",
+        [result.insertId]
+      );
+      return "Employee added successfully!", employee;
+    } catch (err) {
+      console.log({ err });
+    }
+  }
 }
